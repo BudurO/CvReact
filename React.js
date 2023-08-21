ca4a001dbb0bacb207de87cb8c308b852d55f873
BudurO <buduralthubyani@gmail.com>
2023-08-21 03:10:59 +0300

    W5-D1-HW

---
+++ React.js
@@ -0,0 +1,227 @@
+
+
+const ReactBudur = document.getElementById("root");
+let budur = <>
+
+<div className="boxCv">
+<div className="containerHeader">
+<div className="rowName">
+<h1 className="colorGreen">BUDUR ALTHUBYANI</h1>
+<h3 className="colorBlak">FRONTEND DEVELOPER</h3>
+</div>
+<div className="colorGreenLine"></div>
+
+<div className="rowInf">
+<div className="col-icon">
+<span class="material-symbols-outlined">
+mail
+</span>
+<span>    
+buduralthubyani@gmail.com
+</span>
+</div>
+<div className="col-icon">
+<span class="material-symbols-outlined">
+call
+</span>
+<span>      
+0534798594
+</span>
+</div>
+<div className="col-icon">
+<span class="material-symbols-outlined">
+location_on
+</span>
+<span>    
+Saudi Arabia
+</span>
+</div>
+</div>
+</div>
+
+
+
+<div className="container">
+    <div className="rowSummary">
+        <div className="rowProfessional">
+            <div className="col"><h2 className="colorGreen">Professional Summary</h2></div>
+
+        </div>
+        <div className="row">
+            <p>
+            Aspiring Web Developer Passionate About Building Interactive And User-Friendly Websites. Skilled In HTML, CSS, And JavaScript. Actively Learning React And Keeping Up With Web Standards. Able To Translate Designer Mock ups Into Functional Front-End Experiences. Enthusiastic About Picking Up New Skills And Technologies. Eager To Leverage What I'be Learned To Build Impact User Experiences On The Web. I'm A Fast Learner Committed To Continuous Self-Improvement And Delivering Clean, Optimized Front- End Solutions.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div className="containerEducation">
+<div className="row">
+<h2 className="colorGreen">Education</h2>
+</div>
+<div className="rowDate">
+<div className="col">
+<b>Bachelor of Computer Science</b>
+</div>
+<div className="col">
+<p className="Date">2016 - 2021</p>
+
+</div>
+</div>
+<div className="row">
+    <p>Umm Al Qura University - Makkah , Saudi Arabia</p>
+</div>
+</div>
+
+<div className="containerExperience">
+<div className="row">
+<h2 className="colorGreen">Work Experience</h2>
+<div className="rowDate">
+<div className="colnameCompany">
+<b>javascript Bootcamp Trainee</b>
+</div>
+<div className="col">
+<p className="Date">2023 - Present</p>
+</div>
+</div>
+</div>
+<div className="row">
+<p>Tuwaiq Academy 2023 - Riyadh , Saudi Arabia</p>
+</div>
+<div className="rowDate">
+<div className="col">
+<b>Web Developer Intern</b>
+</div>
+<div className="col">
+<p className="Date">2021 - 2021</p>
+</div>
+</div>
+<div className="row">
+<p>Quokka HR - Hong Kong</p>
+<ul>
+    <li>
+    Routinely test websites for usability, speed, and other quality factors. Fix any problems or errors that may appear in the electronic portal and fill security gaps, if any. Ensure that websites are accessible across multiple platforms, including tablets and smartphones.    
+    </li>
+</ul>
+</div>
+</div>
+
+
+
+<div className="containerSkills">
+<div className="row">
+<h2 className="colorGreen">Skills</h2>
+</div>
+<div className="rowAllskills">
+<ul>
+   <div className="rowSkills">
+    <li>HTML5 & CSS3</li>
+   </div>
+
+   <div className="rowSkills">
+    <li>JavaScript</li>
+   </div>
+
+   <div className="rowSkills">
+    <li>Responsive Web Design</li>
+   </div>
+
+   <div className="rowSkills">
+    <li>Front-End Development</li>
+   </div>
+</ul>
+
+<ul>
+<div className="rowSkills">
+   <li>React.js</li>
+   </div>
+
+ <div className="rowSkills">
+ <li>Bootstrap</li>
+   </div>
+
+   <div className="rowSkills">
+   <li>Git & GitHub</li>
+   </div>
+
+   <div className="rowSkills">
+   <li>Design Thinking</li>
+   </div>
+</ul>
+
+</div>
+</div>
+
+<div className="Containercertificates">
+<div className="row">
+<h2 className="colorGreen">CERTIFICATIONS</h2>
+</div>
+<div className="row">
+<div className="rowAllskills">
+    <ul>
+        <div className="rowSkills">
+        <li>React Basics, Meta 2023</li>
+
+        </div>
+        <div className="rowSkills">
+        <li>Version Control, Meta 2023</li>
+
+        </div>
+        <div className="rowSkills">
+        <li>Programming with JavaScript, Meta 2023</li>
+        </div>
+        
+        <div className="rowSkills">
+        <li>Introduction to Front-End Development, Meta 2023</li>
+        </div>
+</ul>
+
+<div className="col">
+
+
+<ul>
+<div className="rowSkills">
+<li>Introduction to Cloud Computing, IBM 2023.</li>
+
+</div>
+<div className="rowSkills">
+<li>HTML5 & CSS3</li>
+
+</div>
+<div className="rowSkills">
+<li>JavaScript</li>
+
+</div>
+<div className="rowSkills">
+<li>Fundamentals of design thinking methodology, Tuwaiq Academy 2023</li>  
+
+</div>
+</ul>
+</div>
+</div>
+
+</div>
+</div>
+
+<div className="containerConnect">
+<div className="row">
+<h2 className="colorGreen">Connect</h2>
+</div>
+<div className="rowConnect">
+<div className="colConnect">
+    <ul className="linkConnect">
+    <li>GitHub : https://github.com/BudurO</li>
+    <li>Linkedin : budur-althubyani</li>
+    <li className="Portfolio">Portfolio : https://buduro.github.io/</li>
+</ul>
+</div>
+
+</div>
+</div>
+
+</div>
+</>
+
+
+
+ReactDOM.render(budur, ReactBudur);
